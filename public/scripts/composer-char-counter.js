82bea41d8224275e41346af1f56c29c116a9e5a0
Krista Kourtessis <krista.kourtessis@telus.com>
2022-12-06 15:21:36 -0800

    implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('textarea.input').on('keyup', onChange);
+
+});
+
+const onChange = function(e) {
+  const input = $(this);
+  const text = input.val();
+  // const text = $('.input').val() //"old js way"
+  const remaining = 140 - text.length;
+
+  const form = input.closest('form');
+  const output = form.find('output');
+
+
+  output.text(remaining);
+  if (remaining < 0) {
+    output.addClass('red');
+  } else {
+    output.removeClass('red');
+  }
+
+};
+
