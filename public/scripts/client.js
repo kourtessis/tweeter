89890f264f71691cd4c83e5e5a27d5dada35f3c7
Krista Kourtessis <krista.kourtessis@telus.com>
2022-12-07 16:51:10 -0800

    fetch tweet with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,30 @@
 * jQuery is already loaded
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
+$(document).ready(function() {
+
+  const $form = $('#tweet-form');
+  loadTweets();
+  $form.on('submit', tweetSubmitted);
+
+});
+
+const tweetSubmitted = (event) => {
+  event.preventDefault();
+  const result = $('#tweet-form').serialize();
+  ///validation if statements - don't forget return line 
+  $.post('/tweets', result, () => {
+    loadTweets();
+  });
+  $('#tweet-text').val('');
+  $('.counter').text(140);
+};
+
+const loadTweets = () => {
+  $.get('/tweets', (tweets) => {
+    renderTweets(tweets);
+  });
+};
 
 
 const createTweetElement = function(tweetObj) {
@@ -10,9 +34,9 @@ const createTweetElement = function(tweetObj) {
   const avatar = tweetObj.user.avatars;
   const handle = tweetObj.user.handle;
   const text = tweetObj.content.text;
-  const date = tweetObj.created_at; ///  TIME AGO will have to come back and format
+  const date = timeago.format(tweetObj.created_at);
 
-  const $tweet = $(`<article class="tweet-spot">
+  const $tweet = `<article>
   <header>
     <div>
       <img src="${avatar}">
@@ -35,78 +59,15 @@ const createTweetElement = function(tweetObj) {
       </div>
     </div>
   </footer>
-</article>`);
+</article>`
   return $tweet;
 };
 
 const renderTweets = function(tweets) {
+  $('#tweet-container').empty();
+
   for (const tweet of tweets) {
     const element = createTweetElement(tweet);
     $('#tweet-container').prepend(element);
   }
-
 };
-
-
-
-
-
-
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
-
-$(document).ready(function() {
-  renderTweets(data);
-
-  const $form = $('#tweet-form');
-  $form.on('submit', (event) => {
-    event.preventDefault();
-    const result = $form.serialize();
-    $.post('/tweets', result, (response) => {
-     
-      const loadTweets = () => {
-        $.get('/tweets', (tweets) =>{
-          renderTweets(tweets)
-        });
-      }
-      loadTweets()
-    });
-  });
-});
